8e1af37b9d504079d7d3c3078335f067492445b6
geforseN <semen.neb@gmail.com>
2022-05-15 18:07:53 +0700

    new file

---
+++ 18dayJS/printAnswers.js
@@ -0,0 +1,5 @@
+import { formatedCountryData, catNames, catsWeight } from "./level.js";
+
+formatedCountryData.forEach(country => console.log(country));
+catNames.forEach(cat => console.log(cat))
+catsWeight.forEach(cat => console.log(cat))
