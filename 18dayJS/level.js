4d8f27cf4b22d18ca40cfff9e8eb7ac8a594bc1a
geforseN <semen.neb@gmail.com>
2022-05-15 16:52:19 +0700

    level 1 and 2 done, level 3 exercise 1 done

--- 18dayJS/level.js
+++ 18dayJS/level.js
@@ -1,60 +1,32 @@
 import { Agent } from "http";
 import fetch from "node-fetch";
-
+import {fetchDataAsyncAwait} from "./promiseFunctions.js";
+import {formatCountryData} from "./functionsForCountriesData.js"
+import { getCatName, getAverageCatWeight} from "./functionsForCatsData.js";
 const countriesAPI = 'https://restcountries.com/v2/all'
 const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 
-function fetchDataPromise(url){
-    fetch(url).then(response => response.json())
-    .then(data => console.log(data))
-    .then(data => data)
-    .catch(error => console.log(error))
-}
-
-const fetchDataAsyncAwait = async (url) => {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        //console.log(data)
-        return data;
-    } catch (err) {
-        console.error(err)
-    } 
-    }
-
-const def = {
-    name: "dede",
-    age: 22
-}
-
+//  fetched data is an array of objects
 let fetchedData = await fetchDataAsyncAwait(countriesAPI)
 
-const fetched = {...fetchedData}
+//  formatedCountryData is an array of strings
+const formatedCountryData = fetchedData.map(country => formatCountryData(country))
 
-console.log(typeof fetchedData, fetchedData)
+formatedCountryData.forEach(country => console.log(country));   //  level 1
 
-console.log(JSON.stringify(fetched))
+fetchedData = await fetchDataAsyncAwait(catsAPI)
+console.log(fetchedData)
+//  меня заставили назвать эту переменную так  
+const catNames = fetchedData.map(cat => getCatName(cat))
 
-console.log(JSON.stringify(fetchedData))
+catNames.forEach(cat => console.log(cat))   //  level 2
 
-// console.log(Object.keys(fetched).forEach(
-//     (country) => {
-//         console.log("country", country, typeof country);
-//         console.log(Object.entries(country))
-//         console.log(Object.keys(country).filter(key => key === "name"));
-//     }
-// )) 
 
-// console.log(JSON.stringify(fetchedData).search((key, value) => key === "name" && value === "Afghanistan"))
+//  Read the cats api and find the average weight of cat in metric unit.
+const catsWeight = fetchedData.map(cat => getAverageCatWeight(cat))
+const totalCatsWeight = catsWeight.reduce((catsWeight, catWeight) => catsWeight += catWeight, 0)
+const averageCatsWeight = totalCatsWeight / catsWeight.length;
+console.log(averageCatsWeight)  // level 3 exercise 1
 
-//let {country, capital, languages, population, area} = fetchedData
-//console.log(country, capital)
 
-//fetchDataPromise(countriesAPI)
-        // const filtered = data.filter(obj => {
-        //     console.log(typeof obj)
-        //     let needfulData = []
-        //     needfulData.push(obj.name)
-        //     needfulData.push(obj.capital)
-        // })
-        // console.log(filtered)
+
