f04d998004efa8b1ee7952da60f001d2ade5f66f
geforseN <semen.neb@gmail.com>
2022-06-16 02:01:49 +0700

    init

---
+++ 25dayJS/25_day_starter/scripts/main.js
@@ -0,0 +1,5 @@
+// import {countries} from "../data/countries_data.js"
+// import {countries_old} from '../data/countries_data_old.js'
+
+console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
