9cea0136b1ec687e3136b8ec0dfbab7ff2d1ad7e
geforseN <semen.neb@gmail.com>
2022-06-16 18:51:51 +0700

    add country table sorted by population

--- 25dayJS/scripts/main.js
+++ 25dayJS/scripts/main.js
@@ -10,15 +10,78 @@ const usefulCountriesData = countriesData.map(({ name, population, languages }) 
   }
 })
 
+
+const MAX_COUNTRIES = 20;
+
+
 //делаем копию массива usefulCountriesData, т.к sort изменяет сортируемый массив
 const sortedFromLowestPopulation = [...usefulCountriesData].sort((a, b) => a.population - b.population)
 const sortedFromHighestPopulation = [...usefulCountriesData].sort((a, b) => b.population - a.population)
 
 // console.log(sortedFromLowestPopulation[0])
-// console.log(sortedFromHighestPopulation[0])
+console.log(sortedFromHighestPopulation[0])
 // console.log(usefulCountriesData[0])
 
 //массив с массивами языков со всех стран
 const languages = usefulCountriesData.map(({ languages }) => languages)
-console.log(languages)
+// console.log(languages)
 
+
+
+const graphContainer = document.body.querySelector(".graph")
+const populationButton = document.body.querySelector(".population")
+console.log(populationButton, graphContainer)
+
+
+
+const table = document.createElement("table")
+graphContainer.append(table)
+
+//  thead START 
+
+const thead = document.createElement("thead") 
+table.append(thead)
+
+const tr = document.createElement("tr")
+thead.append(tr)
+
+const thArray = ["Country", "Popuation"]
+
+for (const country of thArray){
+  console.log(country)
+  const th = document.createElement("th")
+  th.textContent = `${country}`
+  tr.append(th)
+}
+
+//  thead END
+
+// tbody START  tbody > tr > td 'text'
+
+const tbody = document.createElement("tbody")
+table.append(tbody)
+
+for (let i = 0; i < MAX_COUNTRIES; i++){
+  let tr = document.createElement("tr")
+  tbody.append(tr)
+  
+  //very bad
+  let {name, population} = sortedFromHighestPopulation[i];
+  let arr = [name, population]
+
+  for (const value of arr){
+    const td = document.createElement("td")
+    td.textContent = `${value}`
+    tr.append(td)
+  }
+
+}
+
+
+// for (let i = 0; i < 10; i++){
+//   let country = document.createElement("li")
+//   let {name, population} = sortedFromHighestPopulation[i];
+//   country.textContent = `${name} ${population}`
+//   // document.body.appendChild(country)
+//   graphContainer.append(country)
+// }
