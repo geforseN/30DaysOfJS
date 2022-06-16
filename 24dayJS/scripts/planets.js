b56b056df683f0d71ae1a011a063ff659b3d6803
geforseN <semen.neb@gmail.com>
2022-06-16 15:51:22 +0700

    reinit2

---
+++ 24dayJS/scripts/planets.js
@@ -0,0 +1,45 @@
+export const planets = [
+  {
+    name: "Mercury",
+    g: 0.377,
+  },
+  {
+    name: "Venus",
+    g: 0.905,
+  },
+  {
+    name: "Earth",
+    g: 1,
+  },
+  {
+    name: "Moon",
+    g: 0.165,
+  },
+  {
+    name: "Mars",
+    g: 0.379,
+  },
+  {
+    name: "Jupiter",
+    g: 2.528,
+  },
+  {
+    name: "Saturn",
+    g: 1.065,
+  },
+  {
+    name: "Uranus",
+    g: 0.886 ,
+  },
+  {
+    name: "Neptune",
+    g: 1.137,
+  },
+  {
+    name: "Pluto",
+    g: 0.063,
+  },
+]
+
+
+export const G = 9.8
