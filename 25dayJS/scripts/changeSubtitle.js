266c587130cbb66d6ff6f0ecac6f4187b497e089
geforseN <semen.neb@gmail.com>
2022-06-18 13:46:41 +0700

    rename

--- 25dayJS/scripts/changeSubtitle.js
+++ 25dayJS/scripts/changeSubtitle.js
@@ -2,5 +2,7 @@ export default function changeSubtitle(subtitle, number) {
   const oldSubtitleText = subtitle.textContent;
   const newSubtitleText = oldSubtitleText.replace("***", `${number}`)
   subtitle.textContent = newSubtitleText;
+  
   return subtitle;
+  // return subtitle.textContent.replace("***", `${number}`)
 }
