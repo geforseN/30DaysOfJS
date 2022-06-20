32aae7b6022bba63c6cf43bb353038e38e1f3232
geforseN <semen.neb@gmail.com>
2022-06-20 19:20:53 +0700

    remove console.log and comment

--- 25dayJS/scripts/validateObjects.js
+++ 25dayJS/scripts/validateObjects.js
@@ -1,11 +1,8 @@
 import decreaseObjectsEntries from "./decreaseObjectsEntries.js";
-// add sortType , sort(objectsArray)
-// добавить в Object.prototype метод decrease?
 
 export default function validateObjects(objects, sort, keysToFind, maxRows) {
   const decreasedObjects = decreaseObjectsEntries(objects, keysToFind);
   const sortedObjects = sort(decreasedObjects);
-  console.log(sortedObjects)
   const slicedObjects = sortedObjects.slice(0, maxRows);
   const validObjects = slicedObjects;
   
