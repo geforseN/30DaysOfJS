168daceb71bec9eaa0e6340dc636b38d35d6b23e
geforseN <semen.neb@gmail.com>
2022-06-20 19:13:54 +0700

    rework, add sort param

--- 25dayJS/scripts/table/tableInit.js
+++ 25dayJS/scripts/table/tableInit.js
@@ -1,22 +1,25 @@
-import { tableData } from "./tableData.js";
-// //captionInit
+import {populationTableData, languagesTableData} from "./tableData.js";
+// ??captionInit
 import theadInit from "./theadInit.js";
 import tbodyInit from "./tbodyInit.js"
 import validateObjects from "../validateObjects.js"
 
-const { captionContent, headContent, bodyContent, keysToFind, maxRows } = tableData;
 
-function tableInit(captionContent, headContent, bodyContent, keysToFind, maxRows){
+
+function tableInit(tableData){
+  const { captionContent, headContent, bodyContent, sort, keysToFind, maxRows } = tableData;
+
   const tableHead = theadInit(headContent);
 
-  const validObjects = validateObjects(bodyContent, keysToFind, maxRows)
-  const tableBody = tbodyInit(validObjects)
+  const validObjects = validateObjects(bodyContent, sort, keysToFind, maxRows);
+  const tableBody = tbodyInit(validObjects);
 
-  const tableCaption = document.createElement("caption")
-  tableCaption.textContent = captionContent
+  const tableCaption = document.createElement("caption");
+  tableCaption.textContent = captionContent;
 
-
+  console.log(tableCaption);
   return {tableCaption, tableHead, tableBody};
 }
 
-export const tableElement = tableInit(captionContent, headContent, bodyContent, keysToFind, maxRows)
+export const populationElement = tableInit(populationTableData);
+export const languagesElement = tableInit(languagesTableData);
--- 25dayJS/scripts/validateObjects.js
+++ 25dayJS/scripts/validateObjects.js
@@ -1,9 +1,11 @@
 import decreaseObjectsEntries from "./decreaseObjectsEntries.js";
 // add sortType , sort(objectsArray)
 // добавить в Object.prototype метод decrease?
-export default function validateObjects(objects, keysToFind, maxRows) {
+
+export default function validateObjects(objects, sort, keysToFind, maxRows) {
   const decreasedObjects = decreaseObjectsEntries(objects, keysToFind);
-  const sortedObjects = [...decreasedObjects].sort((a, b) => b.population - a.population);
+  const sortedObjects = sort(decreasedObjects);
+  console.log(sortedObjects)
   const slicedObjects = sortedObjects.slice(0, maxRows);
   const validObjects = slicedObjects;
   
